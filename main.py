744d5ea04569c5dfafe90167d2969d4462f2c5d6
PandAstronaut <bastien.longeon@estaca.eu>
2021-06-10 19:01:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,24 @@
 Created on Wed Jun  9 11:05:11 2021
 @author: Bastien Longeon
 
-This code plots:
-· A 4-pannels comparison of the following quotients:
-    Graph 1: Auroral index 1.2: Z/H
-    Graph 2: Auroral index 2: Z/H
-    Graph 3: (Auroral index 2 Z/H) / (Auroral index 1.2 Z/H)
-    Graph 4: Auroral index 1.2 and 2: horizontal
-        In function of time
+For everyday in september from 2011 to 2020, this code plots:
 
-· dBDC in function of dBAC 1 & 2 for the vertical and horizontal component
+    · A 4-pannels comparison of the following quotients:
+        Graph 1: Auroral index 1.2: Z/H
+        Graph 2: Auroral index 2: Z/H
+        Graph 3: (Auroral index 2 Z/H) / (Auroral index 1.2 Z/H)
+        Graph 4: Auroral index 1.2 and 2: horizontal
+            In function of time
+
+    · dBDC in function of dBAC 1 & 2 for the vertical and horizontal component
 """
 import pandas as pd     # pandas is for reading file
 import numpy as np      # instead of math to convert X,Y <--> H,D
 import time
 
-startTime = time.time()
-###              Define the filenames to read              ####
-folder = "maggraphs/"
-filename = "kir201709dsec.sec" # d = definitive
-filePath = folder + filename
+###              Define the file to read              ####
+path = "maggraphs/"
+
 
 ###              Format of the file we read              ####
 #--IAGA2002---*----2----*----3----*----4----*----5----*----6----*----7
@@ -31,36 +30,26 @@ filePath = folder + filename
 
 #The tuples in colnumber are the interval in wich we can find the information
 #linked in the commentary next to each
-colnumber = [(0, 4),     # Year (4 digit)
-             (5, 7),     # Month 01 ... 12
-             (8, 10),    # Day 01 ... 31
-             (11, 13),   # Hour 00 ... 23
-             (14, 16),   # Minute 00 ... 59
-             (17, 19),   # Second 00 ... 59
-             (24, 26),   # Day 000 ... 366
-             (31, 40),   # X [nT]
+colnumber = [(31, 40),   # X [nT]
              (41, 50),   # Y [nT]
-             (51, 60),   # Z [nT]
-             (61, 70)]   # F [nT]
+             (51, 60)]   # Z [nT]
 
 #Creation of the column names for the DataFrame
-station_code = filename[0:3].upper()
-columnx = station_code + '_X'
-columny = station_code + '_Y'
-columnz = station_code + '_Z'
-columnh = station_code + '_H'
-columnf = station_code + '_F'
-colnames = ['year', 'month', 'day', 'hr', 'min', 'sec', 'DOY', columnx, columny, columnz, columnf]
+station = 'KIR'
+columnx = station + '_X'
+columny = station + '_Y'
+columnz = station + '_Z'
+columnh = station + '_H'
+colnames = [columnx, columny, columnz]
 
-"86400s in a day"
+startTime = time.time()
+magdata = pd.read_fwf('maggraphs/kir201709dsec.sec', colspecs=colnumber, names=colnames)
+magdata.to_hdf(path +'test', 'test1', mode='w')
+executionTime = (time.time() - startTime)
+print("Execution time: {0:.2f}s".format(executionTime))
 
-                ###              Creation of the DataFramefor the day              ####
-for i in range(0,2):
-    magdata = pd.read_fwf(filePath, colspecs=colnumber, names=colnames, skiprows = 86400*i, nrows = 86400*(i+1))
-    magdata[columnh] = np.sqrt(magdata[columnx]*magdata[columnx] + magdata[columny]*magdata[columny])
-    del magdata
-
-
-                ###              Execution time              ####
+startTime = time.time()
+new_data = pd.read_hdf(path + 'test')
+new_data[columnh] = np.sqrt(new_data[columnx]*new_data[columnx] + new_data[columny]*new_data[columny])
 executionTime = (time.time() - startTime)
 print("Execution time: {0:.2f}s".format(executionTime))
