b2d12075c4658fbcf039d82f1eb726a060033c90
PandAstronaut <bastien.longeon@estaca.eu>
2021-06-23 15:11:43 +0200

    23/06

--- dataframe_to_HDF5.py
+++ dataframe_to_HDF5.py
@@ -10,6 +10,7 @@ file (.sec is equivalent to txt).
 import pandas as pd     # pandas is for reading file
 import numpy as np      # instead of math to convert X,Y <--> H,D
 import modules.auroral_index_functions as aur
+import os.path
 import time
 startTime = time.time()
 
@@ -17,7 +18,7 @@ colnumber = [(31, 40),   # X [nT]
               (41, 50),   # Y [nT]
               (51, 60)]   # Z [nT]
 
-station = 'KIR'
+station = 'ABK'
 columnx = station + '_X'
 columny = station + '_Y'
 columnz = station + '_Z'
@@ -25,56 +26,56 @@ columnh = station + '_H'
 colnames = [columnx, columny, columnz]
 folder = 'maggraphs/'
 subfolder ='sec files/'
-extension_sec = 'dsec.sec'
+extension_sec = 'psec.sec'
+extension_sec2 = 'qsec.sec'
 extension_hdf = '09sec.hdf5'
 auroral_index = pd.DataFrame()
 
 iterations = 0
-for year in range(2011,2021): # Year: 2011 to 2021
-    # magdata_month = pd.DataFrame() # These two DF will be the ones used to create the HDF5 file
-    # auroral_index_month = pd.DataFrame()
-    # for i in range(0,30):
-        # if i < 9: day = '0' + str(i+1)
-        # else : day = str(i+1)
-        # date = str(year) + '.09.' + day
-        # print('Processing data for the {}'.format(date))
-        filePath = folder + subfolder + station.lower() + str(year) + '09' + extension_sec
+for year in range(2014,2021): # Year: 2011 to 2021
+    magdata_month = pd.DataFrame() # These two DF will be the ones used to create the HDF5 file
+    auroral_index_month = pd.DataFrame()
+    for i in range(0,30):
+        if i < 9: day = '0' + str(i+1)
+        else : day = str(i+1)
+        date = str(year) + '.09.' + day
+        print('Processing data for the {}'.format(date))
+        filePath = folder + subfolder + station.lower() + str(year) + '09' + day + extension_sec
         filename = folder + station.lower() + str(year) + extension_hdf # Corresponds to the path + filename
-
-        # with open(filePath, 'r') as file: #Determination of the number of rows to skip before the datas
-        #     comments_size = 0
-        #     for row in file:
-        #         comments_size += 1
-        #         if "DATE" in row: #the >50 is here to avoid reading the whole documents
-        #             break
-        #         # if comments_size > 50:
-        #         #     print("The end of the header has not been found.")
-        #         #     next(year)
-
-        magdata = pd.read_fwf(filePath, colspecs=colnumber, names=colnames)
+        if os.path.exists(filePath) == False : filePath = folder + subfolder + station.lower() + str(year) + '09' +  day + extension_sec2
+        with open(filePath, 'r') as file: #Determination of the number of rows to skip before the datas
+            comments_size = 0
+            for row in file:
+                comments_size += 1
+                if "DATE" in row: #the >50 is here to avoid reading the whole documents
+                    break
+                if comments_size > 50:
+                    print("The end of the header has not been found.")
+                    break
+        magdata = pd.read_fwf(filePath, skiprows=comments_size ,colspecs=colnumber, names=colnames)
         magdata[columnx].replace(to_replace= 99999.0, value=np.nan, inplace=True)
         magdata[columny].replace(to_replace= 99999.0, value=np.nan, inplace=True)
         magdata[columnz].replace(to_replace= 99999.0, value=np.nan, inplace=True)
         magdata[columnh] = np.sqrt(magdata[columnx]*magdata[columnx] + magdata[columny]*magdata[columny])
-        # magdata_month = magdata_month.append(magdata)
-        if year == 2017:
-            # The anomaly detection has only be found useful for Kiruna 2017 with more than 900 anomalies detected
-            anomalies = 0
-            for j in range(0,2592000):
-                if magdata[columnx].iloc[j] > 40000:
-                    magdata[columnx].iloc[j] = np.nan
-                    anomalies += 1
-                if magdata[columny].iloc[j] > 10000:
-                    magdata[columny].iloc[j] = np.nan
-                    anomalies += 1
-                if magdata[columnz].iloc[j] > 150000:
-                    magdata[columnz].iloc[j] = np.nan
-                    anomalies += 1
-                if magdata[columnh].iloc[j] > 50000:
-                    magdata[columnh].iloc[j] = np.nan
-                    anomalies += 1
-            print('----   {} anomalies detected and corrected   ----'.format(anomalies))
-        magdata.to_hdf(filename, 'data', mode='w')
+        magdata_month = magdata_month.append(magdata)
+        # if year == 2017:
+        #     # The anomaly detection has only be found useful for Kiruna 2017 with more than 900 anomalies detected
+        #     anomalies = 0
+        #     for j in range(0,2592000):
+        #         if magdata[columnx].iloc[j] > 40000:
+        #             magdata[columnx].iloc[j] = np.nan
+        #             anomalies += 1
+        #         if magdata[columny].iloc[j] > 10000:
+        #             magdata[columny].iloc[j] = np.nan
+        #             anomalies += 1
+        #         if magdata[columnz].iloc[j] > 150000:
+        #             magdata[columnz].iloc[j] = np.nan
+        #             anomalies += 1
+        #         if magdata[columnh].iloc[j] > 50000:
+        #             magdata[columnh].iloc[j] = np.nan
+        #             anomalies += 1
+        #     print('----   {} anomalies detected and corrected   ----'.format(anomalies))
+        # magdata.to_hdf(filename, 'data', mode='w')
         ###              Auroral Index 1              ####
         auroral_index['Auroral_Index_1_1_Z'] = aur.auroral_index1_1(magdata, columnz)
         auroral_index['Auroral_Index_1_1_H'] = aur.auroral_index1_1(magdata, columnh)
@@ -103,15 +104,15 @@ for year in range(2011,2021): # Year: 2011 to 2021
 
         ###              (dB/dt)/(<dB_1sec>_1min)             ####
         auroral_index['AC_ratio'] = abs(auroral_index['Auroral_Index_1_1_H'])/auroral_index['Auroral_Index_1_2_H']
-        auroral_index.to_hdf(filename, 'index', mode='a')
-        # auroral_index_month = auroral_index_month.append(auroral_index)
+        # auroral_index.to_hdf(filename, 'index', mode='a')
+        auroral_index_month = auroral_index_month.append(auroral_index)
 
-        iterations += 1
-        executionTime = (time.time() - startTime)
-        if year != 2020:    print('----    Estimated remaining time: {0:.0f}s   ----'.format((executionTime/iterations)*(2020-year)))
+    iterations += 1
+    executionTime = (time.time() - startTime)
+    if year != 2020:    print('----    Estimated remaining time: {0:.0f}s   ----'.format((executionTime/iterations)*(2020-year)))
 
-    # magdata_month.to_hdf(filename, 'data', mode='w')
-    # auroral_index_month.to_hdf(filename, 'index', mode='a')
+    magdata_month.to_hdf(filename, 'data', mode='w')
+    auroral_index_month.to_hdf(filename, 'index', mode='a')
                 ###              Execution time              ####
 executionTime = (time.time() - startTime)
 print("Execution time: {0:.2f}s".format(executionTime))
--- main.py
+++ main.py
@@ -28,7 +28,7 @@ import time
 startTime = time.time()
 
 folder = "maggraphs/"
-station = 'cbb'
+station = 'abk'
 if station == 'kir':
     station_folder = 'Kiruna'
 elif station == 'cbb':
@@ -37,101 +37,100 @@ elif station =='abk': station_folder = 'Abisko'
 extension_hdf = '09sec.hdf5'
 
 iterations = 0
-for year in range(2020,2021): # Year: 11 to 21
-    if year != 2015: # This is to skip the missing year in CBB data
-        for i in range(28,30): # Day in september: 0 to 30
-            gb.initialize()
-            filename = folder + station +  str(year) + extension_hdf
-            if i < 9: day = '0' + str(i+1)
-            else : day = str(i+1)
-            date = str(year) + '.09.' + day
-            print('Processing data for the {}'.format(date))
+for year in range(2014,2021): # Year: 11 to 21
+    for i in range(0,30): # Day in september: 0 to 30
+        gb.initialize()
+        filename = folder + station +  str(year) + extension_hdf
+        if i < 9: day = '0' + str(i+1)
+        else : day = str(i+1)
+        date = str(year) + '.09.' + day
+        print('Processing data for the {}'.format(date))
 
-            magdata = pd.read_hdf(filename, 'data', start=i*86400, stop=(i+1)*86400)
-            auroral_index = pd.read_hdf(filename, 'index', start=i*1440, stop=(i+1)*1440)
-            for k in range(0,1440):
-                if abs(auroral_index['Auroral_Index_2_H'].iloc[k]) < 35 and abs(auroral_index['Auroral_Index_1_2_H'].iloc[k]) < 1:
-                    auroral_index['Auroral_Index_1_2_Z/X'].iloc[k] = np.nan
-                    auroral_index['Auroral_Index_1_2_Z/Y'].iloc[k] = np.nan
-                    auroral_index['DC_ratio_X'].iloc[k] = np.nan
-                    auroral_index['DC_ratio_Y'].iloc[k] = np.nan
-                if auroral_index['Auroral_Index_1_2_H'].iloc[k] < 0.5:
-                    auroral_index['AC_ratio'].iloc[k] = np.nan
-            auroral_index['hour'] = np.linspace(0, 24, 1440)
+        magdata = pd.read_hdf(filename, 'data', start=i*86400, stop=(i+1)*86400)
+        auroral_index = pd.read_hdf(filename, 'index', start=i*1440, stop=(i+1)*1440)
+        for k in range(0,1440):
+            if abs(auroral_index['Auroral_Index_2_H'].iloc[k]) < 35 and abs(auroral_index['Auroral_Index_1_2_H'].iloc[k]) < 1:
+                auroral_index['Auroral_Index_1_2_Z/X'].iloc[k] = np.nan
+                auroral_index['Auroral_Index_1_2_Z/Y'].iloc[k] = np.nan
+                auroral_index['DC_ratio_X'].iloc[k] = np.nan
+                auroral_index['DC_ratio_Y'].iloc[k] = np.nan
+            if auroral_index['Auroral_Index_1_2_H'].iloc[k] < 0.5:
+                auroral_index['AC_ratio'].iloc[k] = np.nan
+        auroral_index['hour'] = np.linspace(0, 24, 1440)
 
-            ###              Plots              ####
-            plt.figure(dpi=300, figsize=(15, 8))
-            gs = gridspec.GridSpec(5, 1)
-            # Subplot 1
-            a1 = plt.subplot(gs[0])
-            (line1, ) = a1.plot(auroral_index['hour'], auroral_index['Auroral_Index_1_2_Z/X'], linewidth=.8, color='orange')
-            line2 = a1.scatter(auroral_index['hour'], auroral_index['Auroral_Index_1_2_Z/Y'], s=.5, facecolors='blue', edgecolors='blue')
-            plt.title('Auroral indexes quotients during Auroral Activity\nfor abs(dB_H) > 35 nT and <dB/dt>_1min > 1 nT/s\n' + date, fontsize=15)
-            # Subplot 2
-            a2 = plt.subplot(gs[1], sharex = a1)
-            (line3, ) = a2.plot(auroral_index['hour'], auroral_index['DC_ratio_X'], color='orange', linewidth=.8)
-            line4 = a2.scatter(auroral_index['hour'], auroral_index['DC_ratio_Y'], s=.5, facecolors='blue', edgecolors='blue')
-            # Subplot 3
-            a3 = plt.subplot(gs[2], sharex = a1)
-            (line5, line6) = a3.plot(auroral_index['hour'], auroral_index['Auroral_Index_2_H'],
-                                      auroral_index['hour'], auroral_index['Auroral_Index_1_2_H'].multiply(100), linewidth=.8)
-            # Subplot 4
-            a4 = plt.subplot(gs[3], sharex = a1)
+        ###              Plots              ####
+        plt.figure(dpi=300, figsize=(15, 8))
+        gs = gridspec.GridSpec(5, 1)
+        # Subplot 1
+        a1 = plt.subplot(gs[0])
+        (line1, ) = a1.plot(auroral_index['hour'], auroral_index['Auroral_Index_1_2_Z/X'], linewidth=.8, color='orange')
+        line2 = a1.scatter(auroral_index['hour'], auroral_index['Auroral_Index_1_2_Z/Y'], s=.5, facecolors='blue', edgecolors='blue')
+        plt.title('Auroral indexes quotients during Auroral Activity\nfor abs(dB_H) > 35 nT and <dB/dt>_1min > 1 nT/s\n' + date, fontsize=15)
+        # Subplot 2
+        a2 = plt.subplot(gs[1], sharex = a1)
+        (line3, ) = a2.plot(auroral_index['hour'], auroral_index['DC_ratio_X'], color='orange', linewidth=.8)
+        line4 = a2.scatter(auroral_index['hour'], auroral_index['DC_ratio_Y'], s=.5, facecolors='blue', edgecolors='blue')
+        # Subplot 3
+        a3 = plt.subplot(gs[2], sharex = a1)
+        (line5, line6) = a3.plot(auroral_index['hour'], auroral_index['Auroral_Index_2_H'],
+                                  auroral_index['hour'], auroral_index['Auroral_Index_1_2_H'].multiply(100), linewidth=.8)
+        # Subplot 4
+        a4 = plt.subplot(gs[3], sharex = a1)
 
-            a4.scatter(auroral_index['hour'], auroral_index['AC_ratio'], s=.5, label = "abs(dB/dt)_H/<dB_1sec>_H")
+        a4.scatter(auroral_index['hour'], auroral_index['AC_ratio'], s=.5, label = "abs(dB/dt)_H/<dB_1sec>_H")
 
-            # Subplot 5
-            a5 = plt.subplot(gs[4], sharex = a1)
-            (line8, line9) = a5.plot(auroral_index['hour'], auroral_index['Auroral_Index_1_1_H'],
-                                      auroral_index['hour'], auroral_index['Auroral_Index_1_2_H'], linewidth=.8)
+        # Subplot 5
+        a5 = plt.subplot(gs[4], sharex = a1)
+        (line8, line9) = a5.plot(auroral_index['hour'], auroral_index['Auroral_Index_1_1_H'],
+                                  auroral_index['hour'], auroral_index['Auroral_Index_1_2_H'], linewidth=.8)
 
-            # Plot setup
-            plt.setp(a1.get_xticklabels(), visible=False)
-            plt.setp(a2.get_xticklabels(), visible=False)
-            plt.setp(a3.get_xticklabels(), visible=False)
-            plt.setp(a4.get_xticklabels(), visible=False)
-            a1.grid()
-            a2.grid()
-            a3.grid()
-            a1.legend([line2, line1], ['dBAC_Z/dBAC_X', 'dBAC_Z/dBAC_Y'])
-            a2.legend([line4, line3], ['log10(dBDC_Z/ dBDC_X)', 'log10(dBDC_Z/ dBDC_Y)'])
-            a3.legend([line5, line6], ["dBDC_H", "dBAC_H x 100"])
-            a4.legend()
-            a5.legend([line8, line9],["(dB/dt)_H","<dB_1sec>_H"])
-            a1.set_ylim([0, 5])
-            # a2.set_ylim([-2, 2])
-            plt.xlim([0, 24])
-            plt.xticks(np.arange(0,25,4)) # Show the last tick of the x-axes to see the 24-hours mark
-            a2.yaxis.set_minor_locator(AutoMinorLocator(1))
-            a5.xaxis.set_minor_locator(AutoMinorLocator(5))
-            a5.tick_params('x', which='minor', length=6)
-            a5.tick_params('x', which='major', length=8)
-            plt.subplots_adjust(hspace=0.14) # Vertical gap between subplots
-            plt.savefig('D:/Documents/GitHub/auroral_indexes/Plots/' + station_folder + '/Pannels/' + str(year) + '/Pannels - ' + date + '.png', dpi=300, bbox_inches='tight')
-            plt.show()
+        # Plot setup
+        plt.setp(a1.get_xticklabels(), visible=False)
+        plt.setp(a2.get_xticklabels(), visible=False)
+        plt.setp(a3.get_xticklabels(), visible=False)
+        plt.setp(a4.get_xticklabels(), visible=False)
+        a1.grid()
+        a2.grid()
+        a3.grid()
+        a1.legend([line2, line1], ['dBAC_Z/dBAC_X', 'dBAC_Z/dBAC_Y'])
+        a2.legend([line4, line3], ['log10(dBDC_Z/ dBDC_X)', 'log10(dBDC_Z/ dBDC_Y)'])
+        a3.legend([line5, line6], ["dBDC_H", "dBAC_H x 100"])
+        a4.legend()
+        a5.legend([line8, line9],["(dB/dt)_H","<dB_1sec>_H"])
+        a1.set_ylim([0, 5])
+        # a2.set_ylim([-2, 2])
+        plt.xlim([0, 24])
+        plt.xticks(np.arange(0,25,4)) # Show the last tick of the x-axes to see the 24-hours mark
+        a2.yaxis.set_minor_locator(AutoMinorLocator(1))
+        a5.xaxis.set_minor_locator(AutoMinorLocator(5))
+        a5.tick_params('x', which='minor', length=6)
+        a5.tick_params('x', which='major', length=8)
+        plt.subplots_adjust(hspace=0.14) # Vertical gap between subplots
+        plt.savefig('D:/Documents/GitHub/auroral_indexes/Plots/' + station_folder + '/Pannels/' + str(year) + '/Pannels - ' + date + '.png', dpi=300, bbox_inches='tight')
+        plt.show()
 
-            for l in range(0,1440): # A criterion is applied here and not before because it would compromise the first plot
-                if abs(auroral_index['Auroral_Index_2_H'].iloc[l]) < 35 and abs(auroral_index['Auroral_Index_1_2_H'].iloc[l]) < 1:
-                    auroral_index['Auroral_Index_2_H'].iloc[l] = np.nan
-                    auroral_index['Auroral_Index_2_Z'].iloc[l] = np.nan
-            # These replacements avoid log10(0) errors without modifying the plots
-            auroral_index['Auroral_Index_1_1_H'].replace(to_replace=0, value=np.nan, inplace=True)
-            auroral_index['Auroral_Index_1_2_H'].replace(to_replace=0, value=np.nan, inplace=True)
-            auroral_index['Auroral_Index_2_H'].replace(to_replace=0, value=np.nan, inplace=True)
-            auroral_index['Auroral_Index_1_1_Z'].replace(to_replace=0, value=np.nan, inplace=True)
-            auroral_index['Auroral_Index_1_2_Z'].replace(to_replace=0, value=np.nan, inplace=True)
-            auroral_index['Auroral_Index_2_Z'].replace(to_replace=0, value=np.nan, inplace=True)
+        for l in range(0,1440): # A criterion is applied here and not before because it would compromise the first plot
+            if abs(auroral_index['Auroral_Index_2_H'].iloc[l]) < 35 and abs(auroral_index['Auroral_Index_1_2_H'].iloc[l]) < 1:
+                auroral_index['Auroral_Index_2_H'].iloc[l] = np.nan
+                auroral_index['Auroral_Index_2_Z'].iloc[l] = np.nan
+        # These replacements avoid log10(0) errors without modifying the plots
+        auroral_index['Auroral_Index_1_1_H'].replace(to_replace=0, value=np.nan, inplace=True)
+        auroral_index['Auroral_Index_1_2_H'].replace(to_replace=0, value=np.nan, inplace=True)
+        auroral_index['Auroral_Index_2_H'].replace(to_replace=0, value=np.nan, inplace=True)
+        auroral_index['Auroral_Index_1_1_Z'].replace(to_replace=0, value=np.nan, inplace=True)
+        auroral_index['Auroral_Index_1_2_Z'].replace(to_replace=0, value=np.nan, inplace=True)
+        auroral_index['Auroral_Index_2_Z'].replace(to_replace=0, value=np.nan, inplace=True)
 
-            plotter.plot_2D_scatter(auroral_index, 'Auroral_Index_1_1_H', 'Auroral_Index_2_H', 'Auroral_Index_1_2_H',
-                'Auroral_Index_2_H', 'Horizontal Auroral index 2 = f(Auroral index 1(1) & 1(2))\nfor abs(dB_H) > 35 nT and <dB/dt>_1min > 1 nT/s\n'+ date,
-                '[nT/s]','log(dB) [nT]')
-            plt.savefig('D:/Documents/GitHub/auroral_indexes/Plots/' + station_folder + '/Distribution/' + str(year) + '/Horizontal distribution - ' + date + '.png', dpi=300, bbox_inches='tight')
-            plt.show()
-            plotter.plot_2D_scatter(auroral_index, 'Auroral_Index_1_1_Z', 'Auroral_Index_2_Z', 'Auroral_Index_1_2_Z',
-                        'Auroral_Index_2_Z', 'Vertical Auroral index 2 = f(Auroral index 1(1) & 1(2))\nfor abs(dB_H) > 35 nT and <dB/dt>_1min > 1 nT/s\n'+ date,
-                        '[nT/s]','log(dB) [nT]')
-            plt.savefig('D:/Documents/GitHub/auroral_indexes/Plots/' + station_folder + '/Distribution/' + str(year) + '/Vertical distribution - ' + date + '.png', dpi=300, bbox_inches='tight')
-            plt.show()
+        plotter.plot_2D_scatter(auroral_index, 'Auroral_Index_1_1_H', 'Auroral_Index_2_H', 'Auroral_Index_1_2_H',
+            'Auroral_Index_2_H', 'Horizontal Auroral index 2 = f(Auroral index 1(1) & 1(2))\nfor abs(dB_H) > 35 nT and <dB/dt>_1min > 1 nT/s\n'+ date,
+            '[nT/s]','log(dB) [nT]')
+        plt.savefig('D:/Documents/GitHub/auroral_indexes/Plots/' + station_folder + '/Distribution/' + str(year) + '/Horizontal distribution - ' + date + '.png', dpi=300, bbox_inches='tight')
+        plt.show()
+        plotter.plot_2D_scatter(auroral_index, 'Auroral_Index_1_1_Z', 'Auroral_Index_2_Z', 'Auroral_Index_1_2_Z',
+                    'Auroral_Index_2_Z', 'Vertical Auroral index 2 = f(Auroral index 1(1) & 1(2))\nfor abs(dB_H) > 35 nT and <dB/dt>_1min > 1 nT/s\n'+ date,
+                    '[nT/s]','log(dB) [nT]')
+        plt.savefig('D:/Documents/GitHub/auroral_indexes/Plots/' + station_folder + '/Distribution/' + str(year) + '/Vertical distribution - ' + date + '.png', dpi=300, bbox_inches='tight')
+        plt.show()
     iterations += 1
     executionTime = (time.time() - startTime)
     if year != 2020:    print('----    Estimated remaining time: {0:.0f}s   ----'.format((executionTime/iterations)*(2020-year)))
